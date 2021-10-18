074fefb4e9636a797729d3f6548234557b8a5127
Eno Compton <enocom@google.com>
2021-10-18 10:54:29 -0600

    Fix build tag issue

--- e2e_postgres_test.go
+++ e2e_postgres_test.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !skip_e2e
-// +build !skip_postgres
-
 //go:build !skip_e2e && !skip_postgres
 // +build !skip_e2e,!skip_postgres
 
