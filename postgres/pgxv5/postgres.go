c3ddc03bad9d3454ce65dc1a51d417caef1d688b
Eno Compton <enocom@google.com>
2022-11-23 14:06:06 -0700

    build on go 1.18 and later

--- postgres/pgxv5/postgres.go
+++ postgres/pgxv5/postgres.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build go1.18
+
 // Package pgxv5 provides a Cloud SQL Postgres driver that uses pgx v5 and works
 // with the database/sql package.
 package pgxv5
--- postgres/pgxv5/postgres_test.go
+++ postgres/pgxv5/postgres_test.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build go1.18
+
 package pgxv5_test
 
 import (
