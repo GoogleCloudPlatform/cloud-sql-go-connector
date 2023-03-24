5c32a85e29d954bd0ed50d5062d14f326797b13e
Eno Compton <enocom@google.com>
2023-03-24 13:39:29 -0600

    chore: fix mock setup in test
    Fixes #317.

--- dialer_test.go
+++ dialer_test.go
@@ -77,7 +77,11 @@ func TestDialerCanConnectToInstance(t *testing.T) {
 
 func TestDialWithAdminAPIErrors(t *testing.T) {
 	inst := mock.NewFakeCSQLInstance("my-project", "my-region", "my-instance")
-	svc, cleanup, err := mock.NewSQLAdminService(context.Background())
+	svc, cleanup, err := mock.NewSQLAdminService(
+		context.Background(),
+		mock.InstanceGetSuccess(inst, 2),
+		mock.CreateEphemeralSuccess(inst, 2),
+	)
 	if err != nil {
 		t.Fatalf("failed to init SQLAdminService: %v", err)
 	}
