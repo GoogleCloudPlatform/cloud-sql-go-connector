fb2e6685063e1edd88b6dda7bf4ef138dfba8dbf
jackwotherspoon <jackwoth@google.com>
2024-03-06 01:43:53 +0000

    chore: fix tests

--- dialer_test.go
+++ dialer_test.go
@@ -189,10 +189,10 @@ func fakeServiceAccount(ud string) []byte {
 		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 		"token_uri": "https://oauth2.googleapis.com/token",
 		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/email%40example.com,"
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/email%40example.com"
 	`
 	if ud != "" {
-		sa = sa + fmt.Sprintf(`	"universe_domain": "%s"`, ud)
+		sa = sa + fmt.Sprintf(`, "universe_domain": "%s"`, ud)
 	}
 	return []byte(fmt.Sprintf(`{ %s }`, sa))
 }
