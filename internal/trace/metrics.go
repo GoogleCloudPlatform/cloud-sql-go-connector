6a2245231d65a5b98f92e94115ac5b1059542dfa
jackwotherspoon <jackwoth@google.com>
2024-08-08 14:33:26 +0000

    chore: add bytes sent and received to reported metrics

--- internal/trace/metrics.go
+++ internal/trace/metrics.go
@@ -104,6 +104,20 @@ var (
 		Aggregation: view.Count(),
 		TagKeys:     []tag.Key{keyInstance, keyDialerID, keyErrorCode},
 	}
+	bytesSentView = &view.View{
+		Name:        "cloudsqlconn//bytes_sent_count",
+		Measure:     mBytesSent,
+		Description: "The number of bytes sent to Cloud SQL",
+		Aggregation: view.LastValue(),
+		TagKeys:     []tag.Key{keyInstance, keyDialerID},
+	}
+	bytesReceivedView = &view.View{
+		Name:        "cloudsqlconn//bytes_received_count",
+		Measure:     mBytesReceived,
+		Description: "The number of bytes received from Cloud SQL",
+		Aggregation: view.LastValue(),
+		TagKeys:     []tag.Key{keyInstance, keyDialerID},
+	}
 
 	registerOnce sync.Once
 	registerErr  error
@@ -120,6 +134,8 @@ func InitMetrics() error {
 			dialFailureView,
 			refreshCountView,
 			failedRefreshCountView,
+			bytesSentView,
+			bytesReceivedView,
 		); rErr != nil {
 			registerErr = fmt.Errorf("failed to initialize metrics: %v", rErr)
 		}
@@ -165,6 +181,18 @@ func RecordRefreshResult(ctx context.Context, instance, dialerID string, err err
 		return
 	}
 	stats.Record(ctx, mSuccessfulRefresh.M(1))
+}
+
+// RecordBytesSent reports the number of bytes sent to Cloud SQL
+func RecordBytesSent(ctx context.Context, num int64, instance, dialerID string) {
+	ctx, _ = tag.New(ctx, tag.Upsert(keyInstance, instance), tag.Upsert(keyDialerID, dialerID))
+	stats.Record(ctx, mBytesSent.M(num))
+}
+
+// RecordBytesReceived reports the number of bytes received from Cloud SQL
+func RecordBytesReceived(ctx context.Context, num int64, instance, dialerID string) {
+	ctx, _ = tag.New(ctx, tag.Upsert(keyInstance, instance), tag.Upsert(keyDialerID, dialerID))
+	stats.Record(ctx, mBytesReceived.M(num))
 }
 
 // errorCode returns an error code as given from the SQL Admin API, provided the
